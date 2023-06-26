5ab0ef4667fd437fa23f20f932791ac09de13136
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-26 13:29:02 +0100

    Add files via upload

--- builder.sh
+++ builder.sh
@@ -7,7 +7,7 @@ rm *.dll
 nasm -felf32 -o boot.o boot.S
 mcs -unsafe kernel.cs -o kernel.exe
 mono --aot=asmonly kernel.exe
-sed 's/local/globl/g' kernel.exe.s > kernel.s.s
+sed 's/local os_oss_kernel_main/globl os_oss_kernel_main/g' kernel.exe.s > kernel.s.s
 as kernel.s.s -o kernel.o 
 gcc link.ld boot.o kernel.o -o kernel.elf -nostdlib
 objcopy kernel.elf -O binary kernel.bin
