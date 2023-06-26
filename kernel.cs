f45126b315efbd614c405c201c75a31e85cb618f
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-26 14:36:26 +0100

    Add files via upload

--- kernel.cs
+++ kernel.cs
@@ -2,17 +2,140 @@ namespace os
 {
     class oss
     {
-        
-        unsafe static void kernel_main()
+        unsafe static void printc(byte b,int loc)
         {
+        		   int i=loc;
+			   byte *fbp=(byte* )i;
+			   	*((byte *)(fbp)) =(byte)b;
+			   	*((byte *)(fbp+1)) =(byte)0x17;		  
+        }
+        static void kernel_main()
+        {
+			   int i=0xb8000;
+			   byte b=65;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   			   
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;			   
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;			   
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;			   
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;			   
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;			   
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
+			   printc(b,i);
+			   i=i+2;
+			   b++;
 			   
-			   byte *fbp=(byte* )0xb8000;
-			   *((byte *)(fbp)) =(byte)65;
-			   *((byte *)(fbp+1)) =(byte)0x17;
 			   
+			   	
+			   
+			  
         }
  
-        unsafe static void Main()
+        static void Main()
         {
          kernel_main();
         }
